b0e8ec6948cbb82dca98a39ed71571662f36ec31
FRancisco <blender.webservices@gmail.com>
2026-08-17 22:47:15 -0600

    chore: update privacy policy and terms and conditions URLs

--- lib/utils/app_config.dart
+++ lib/utils/app_config.dart
@@ -1,9 +1,9 @@
 class AppConfig {
   /// URL para la Política de Privacidad de la aplicación.
-  static const String privacyPolicyUrl = 'https://solutionsfactorystudio.com/privacidad';
+  static const String privacyPolicyUrl = 'https://solutions-factory-studio.app/portfolio/1/privacy';
 
   /// URL para los Términos y Condiciones de la aplicación.
-  static const String termsAndConditionsUrl = 'https://solutionsfactorystudio.com/terminos';
+  static const String termsAndConditionsUrl = 'https://solutions-factory-studio.app/portfolio/1/terms';
 
   /// Si el sonido de clic de las teclas está activo.
   static bool clickSoundEnabled = true;
